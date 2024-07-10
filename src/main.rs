b60a59a1c3ef7f2d921b0f865db740277ff1e3a7
Aleksandar Lazic <al-git001@none.at>
2024-07-10 20:32:27 +0200

    remove jemalloc usage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-#[cfg(not(target_env = "msvc"))]
-use tikv_jemallocator::Jemalloc;
-
-#[cfg(not(target_env = "msvc"))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
-
 mod config;
 mod servers;
 mod upstreams;
